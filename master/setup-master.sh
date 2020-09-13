4197a7287b4c65f3054b9fde379fba961fe42b84
danperezsan <dan@mesoform.com>
2020-09-13 13:39:29 +0200

    Change psql create role replication command

--- master/setup-master.sh
+++ master/setup-master.sh
@@ -2,16 +2,16 @@
 
 PG_REP_PASSWORD=$(cat ${PG_REP_PASSWORD_FILE})
 
-echo "host replication all ${HBA_ADDRESS} md5" >> "$PGDATA/pg_hba.conf"
-
 set -e
-psql -v ON_ERROR_STOP=1 -U $POSTGRES_USER -d $POSTGRES_DB -c "CREATE USER $PG_REP_USER REPLICATION LOGIN CONNECTION LIMIT 100 ENCRYPTED PASSWORD '$PG_REP_PASSWORD'"
+psql -v ON_ERROR_STOP=1 -U $POSTGRES_USER -d $POSTGRES_DB -c "CREATE ROLE $PG_REP_USER WITH REPLICATION PASSWORD '$PG_REP_PASSWORD' LOGIN"
+
+echo "host replication all ${HBA_ADDRESS} md5" >> "$PGDATA/pg_hba.conf"
 
 # replication specific configuration
 echo "wal_level = hot_standby" >> $PGDATA/postgresql.conf
 echo "archive_mode = on" >> $PGDATA/postgresql.conf
 echo "archive_command = 'cd .'" >> $PGDATA/postgresql.conf
-echo "max_wal_senders = 8" >> $PGDATA/postgresql.conf
-echo "wal_keep_segments = 8" >> $PGDATA/postgresql.conf
+echo "max_wal_senders = 5" >> $PGDATA/postgresql.conf
+echo "wal_keep_segments = 32" >> $PGDATA/postgresql.conf
 echo "hot_standby = on" >> $PGDATA/postgresql.conf
 echo "synchronous_standby_names = '*'" >> $PGDATA/postgresql.conf
