21809c47c192b967e855c1aa0b845273b9ec76f0
danperezsan <dan@mesoform.com>
2020-09-14 23:18:34 +0200

    Replace entrypoint with custom docker-entrypoint script

--- master/setup-master.sh
+++ master/setup-master.sh
@@ -2,10 +2,27 @@
 
 PG_REP_PASSWORD=$(cat ${PG_REP_PASSWORD_FILE})
 
-set -e
-psql -v ON_ERROR_STOP=1 -U $POSTGRES_USER -d $POSTGRES_DB -c "CREATE ROLE $PG_REP_USER WITH REPLICATION PASSWORD '$PG_REP_PASSWORD' LOGIN"
+config_file=$PGDATA/postgresql.conf
+
+# Check if configuration file exists. If not, it probably means that database is not initialized yet
+if [ ! -f $config_file ]; then
+  return
+fi
+
+# Reinitialize config
+sed -i "s/wal_level =.*$//g" $config_file
+sed -i "s/archive_mode =.*$//g" $config_file
+sed -i "s/archive_command =.*$//g" $config_file
+sed -i "s/archive_timeout =.*$//g" $config_file
+sed -i "s/max_wal_senders =.*$//g" $config_file
+sed -i "s/wal_keep_segments =.*$//g" $config_file
+sed -i "s/hot_standby =.*$//g" $config_file
+sed -i "s/synchronous_standby_names =.*$//g" $config_file
 
 echo "host replication all ${HBA_ADDRESS} md5" >> "$PGDATA/pg_hba.conf"
+
+set -e
+psql -v ON_ERROR_STOP=1 -U $POSTGRES_USER -d $POSTGRES_DB -c "CREATE ROLE $PG_REP_USER WITH REPLICATION PASSWORD '$PG_REP_PASSWORD' LOGIN"
 
 # replication specific configuration
 echo "wal_level = hot_standby" >> $PGDATA/postgresql.conf
