7ab77edd06b13c3b202fbee188214d14b60f9113
Gareth Brown <gaz@gbmail.me>
2020-09-21 18:02:10 +0100

    docker_temp_server_start without arguments

--- master/entrypoint.sh
+++ master/entrypoint.sh
@@ -29,7 +29,7 @@ function update_conf () {
     source /usr/local/bin/docker-entrypoint.sh
 
     docker_setup_env
-    docker_temp_server_start "$@"
+    docker_temp_server_start
     bash /docker-entrypoint-initdb.d/setup-master.sh
     docker_temp_server_stop
   fi
